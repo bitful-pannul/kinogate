49f740c52e906d233bd947dabca6e25f29ed9e5f
bitful-pannul <bitful-pancreas@protonmail.com>
2024-02-27 15:52:35 -0300

    init scaffolding for ui

--- kinogate/src/lib.rs
+++ kinogate/src/lib.rs
@@ -1,7 +1,8 @@
 use frankenstein::{ChatId, SendMessageParams, TelegramApi, UpdateContent::Message as TgMessage};
+use kinode::process::standard::get_blob;
 use kinode_process_lib::{
     await_message, call_init,
-    http::{bind_http_path, serve_ui},
+    http::{bind_http_path, serve_ui, HttpServerRequest},
     println, Address, Message,
 };
 use serde::{Deserialize, Serialize};
@@ -23,6 +24,7 @@ pub struct Initialize {
     contract: String,
     min_amount: u64,
     priv_chat_id: u64,
+    url: String,
 }
 
 fn handle_message(
@@ -32,6 +34,10 @@ fn handle_message(
     info: &Initialize,
 ) -> anyhow::Result<()> {
     let message = await_message()?;
+
+    if message.source().process == "http_server:distro:sys" {
+        handle_http_message(message.clone(), api, worker, info)?
+    }
 
     match message {
         Message::Response { .. } => {
@@ -76,8 +82,18 @@ fn handle_message(
 
                             match text.as_str() {
                                 "/start" => {
-                                    let text = format!("hello, to enter this chat, you need at least {:?} of {:?} on chainId {:?} to enter, if you have that, sign the message to get invite link.", info.min_amount, info.contract, info.chain);
+                                    let text = format!("hello, to enter this chat, you need at least {:?} of {:?} on chainId {:?} to enter, if you have that, sign the simple message to get invite link.", info.min_amount, info.contract, info.chain);
                                     params.text = text;
+                                    api.send_message(&params)?;
+
+                                    // maybe save user into state here?
+                                    let chat_id = msg.chat.id;
+
+                                    let link = format!(
+                                        "{}/kinogate:kinogate:template.os/?chat_id={}",
+                                        info.url, chat_id
+                                    );
+                                    params.text = link;
                                     api.send_message(&params)?;
                                 }
                                 _ => {
@@ -100,6 +116,61 @@ fn handle_message(
     Ok(())
 }
 
+fn handle_http_message(
+    message: Message,
+    api: &Api,
+    worker: &Address,
+    info: &Initialize,
+) -> anyhow::Result<()> {
+    match message {
+        Message::Request {
+            ref source,
+            ref body,
+            ..
+        } => {
+            let http_req = serde_json::from_slice::<HttpServerRequest>(body)?;
+            if let HttpServerRequest::Http(incoming) = http_req {
+                if incoming.method()? == "POST" {
+                    let chat_id = incoming
+                        .query_params()
+                        .get("chat_id")
+                        .cloned()
+                        .unwrap_or_else(|| "".to_string());
+
+                    let mut params = SendMessageParams {
+                        chat_id: ChatId::Integer(chat_id.parse::<i64>()?),
+                        disable_notification: None,
+                        entities: None,
+                        link_preview_options: None,
+                        message_thread_id: None,
+                        parse_mode: None,
+                        text: "temp".to_string(),
+                        protect_content: None,
+                        reply_markup: None,
+                        reply_parameters: None,
+                    };
+
+                    let blob = get_blob();
+                    // get signature, check for that address actually owns the amount,
+                    // generate invite link, and send!
+                    if let Some(blob) = blob {
+                        params.text =
+                            "we have something in blob, vverifying, u get let in.".to_string();
+                        api.send_message(&params)?;
+                    } else {
+                        println!("http error, no signature sent...");
+                        return Ok(());
+                    }
+                }
+            }
+        }
+        _ => {
+            return Err(anyhow::anyhow!("unexpected message: {:?}", message));
+        }
+    }
+    Ok(())
+}
+
 call_init!(init);
 
 fn init(our: Address) {
@@ -111,14 +182,7 @@ fn init(our: Address) {
     let (api, worker) = init_tg_bot(our.clone(), &info.tg_token, None).unwrap();
 
     // serve ui and bind http post path.
-    serve_ui(
-        &our,
-        &format!("{}/pkg/ui/", our.package_id()),
-        false,
-        false,
-        vec![],
-    )
-    .unwrap();
+    serve_ui(&our, "ui", false, false, vec!["/"]).unwrap();
 
     bind_http_path("/signature", false, false).unwrap();
 
