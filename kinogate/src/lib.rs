af50dca130fba89fcfb55912a071ea3f7c4f2af8
bitful-pannul <bitful-pancreas@protonmail.com>
2024-02-27 15:01:16 -0300

    init scaffolding

--- kinogate/src/lib.rs
+++ kinogate/src/lib.rs
@@ -1,15 +1,12 @@
-use std::collections::HashMap;
-use std::str::FromStr;
-
+use frankenstein::{ChatId, SendMessageParams, TelegramApi, UpdateContent::Message as TgMessage};
 use kinode_process_lib::{
-    await_message, call_init, get_blob,
-    http::{
-        bind_http_path, bind_ws_path, send_response, send_ws_push, serve_ui, HttpServerRequest,
-        IncomingHttpRequest, StatusCode, WsMessageType,
-    },
-    println, Address, LazyLoadBlob, Message, ProcessId, Request, Response,
+    await_message, call_init,
+    http::{bind_http_path, serve_ui},
+    println, Address, Message,
 };
 use serde::{Deserialize, Serialize};
+mod tg_api;
+use tg_api::{init_tg_bot, Api, TgResponse};
 
 wit_bindgen::generate!({
     path: "wit",
@@ -20,271 +17,113 @@ wit_bindgen::generate!({
 });
 
 #[derive(Debug, Serialize, Deserialize)]
-enum ChatRequest {
-    Send { target: String, message: String },
-    History,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-enum ChatResponse {
-    Ack,
-    History { messages: MessageArchive },
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-struct ChatMessage {
-    author: String,
-    content: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct NewMessage {
-    chat: String,
-    author: String,
-    content: String,
-}
-
-type MessageArchive = HashMap<String, Vec<ChatMessage>>;
-
-fn handle_http_server_request(
-    our: &Address,
-    message_archive: &mut MessageArchive,
-    our_channel_id: &mut u32,
-    source: &Address,
-    body: &[u8],
-) -> anyhow::Result<()> {
-    let Ok(server_request) = serde_json::from_slice::<HttpServerRequest>(body) else {
-        // Fail silently if we can't parse the request
-        return Ok(());
-    };
-
-    match server_request {
-        HttpServerRequest::WebSocketOpen { channel_id, .. } => {
-            // Set our channel_id to the newly opened channel
-            // Note: this code could be improved to support multiple channels
-            *our_channel_id = channel_id;
-        }
-        HttpServerRequest::WebSocketPush { .. } => {
-            let Some(blob) = get_blob() else {
-                return Ok(());
-            };
-
-            handle_chat_request(
-                our,
-                message_archive,
-                our_channel_id,
-                source,
-                &blob.bytes,
-                false,
-            )?;
-        }
-        HttpServerRequest::WebSocketClose(_channel_id) => {}
-        HttpServerRequest::Http(request) => {
-            match request.method()?.as_str() {
-                // Get all messages
-                "GET" => {
-                    let mut headers = HashMap::new();
-                    headers.insert("Content-Type".to_string(), "application/json".to_string());
-
-                    send_response(
-                        StatusCode::OK,
-                        Some(headers),
-                        serde_json::to_vec(&ChatResponse::History {
-                            messages: message_archive.clone(),
-                        })
-                        .unwrap(),
-                    );
-                }
-                // Send a message
-                "POST" => {
-                    let Some(blob) = get_blob() else {
-                        return Ok(());
-                    };
-                    handle_chat_request(
-                        our,
-                        message_archive,
-                        our_channel_id,
-                        source,
-                        &blob.bytes,
-                        true,
-                    )?;
-
-                    // Send an http response via the http server
-                    send_response(StatusCode::CREATED, None, vec![]);
-                }
-                _ => {
-                    // Method not allowed
-                    send_response(StatusCode::METHOD_NOT_ALLOWED, None, vec![]);
-                }
-            }
-        }
-    };
-
-    Ok(())
-}
-
-fn handle_chat_request(
-    our: &Address,
-    message_archive: &mut MessageArchive,
-    channel_id: &mut u32,
-    source: &Address,
-    body: &[u8],
-    is_http: bool,
-) -> anyhow::Result<()> {
-    let Ok(chat_request) = serde_json::from_slice::<ChatRequest>(body) else {
-        // Fail silently if we can't parse the request
-        return Ok(());
-    };
-
-    match chat_request {
-        ChatRequest::Send {
-            ref target,
-            ref message,
-        } => {
-            // counterparty will be the other node in the chat with us
-            let (counterparty, author) = if target == &our.node {
-                (&source.node, source.node.clone())
-            } else {
-                (target, our.node.clone())
-            };
-
-            // If the target is not us, send a request to the target
-
-            if target != &our.node {
-                println!("new message from {}: {}", source.node, message);
-
-                match Request::new()
-                    .target(Address {
-                        node: target.clone(),
-                        process: ProcessId::from_str("kinogate:kinogate:template.os")?,
-                    })
-                    .body(body)
-                    .send_and_await_response(5)
-                {
-                    Ok(_) => {}
-                    Err(e) => {
-                        println!("testing: send request error: {:?}", e);
-                        return Ok(());
-                    }
-                };
-            }
-
-            // Retreive the message archive for the counterparty, or create a new one if it doesn't exist
-            let messages = match message_archive.get_mut(counterparty) {
-                Some(messages) => messages,
-                None => {
-                    message_archive.insert(counterparty.clone(), Vec::new());
-                    message_archive.get_mut(counterparty).unwrap()
-                }
-            };
-
-            let new_message = ChatMessage {
-                author: author.clone(),
-                content: message.clone(),
-            };
-
-            // If this is an HTTP request, handle the response in the calling function
-            if is_http {
-                // Add the new message to the archive
-                messages.push(new_message);
-                return Ok(());
-            }
-
-            // If this is not an HTTP request, send a response to the other node
-            Response::new()
-                .body(serde_json::to_vec(&ChatResponse::Ack).unwrap())
-                .send()
-                .unwrap();
-
-            // Add the new message to the archive
-            messages.push(new_message);
-
-            // Generate a blob for the new message
-            let blob = LazyLoadBlob {
-                mime: Some("application/json".to_string()),
-                bytes: serde_json::json!({
-                    "NewMessage": NewMessage {
-                        chat: counterparty.clone(),
-                        author,
-                        content: message.clone(),
-                    }
-                })
-                .to_string()
-                .as_bytes()
-                .to_vec(),
-            };
-
-            // Send a WebSocket message to the http server in order to update the UI
-            send_ws_push(
-                channel_id.clone(),
-                WsMessageType::Text,
-                blob,
-            );
-        }
-        ChatRequest::History => {
-            // If this is an HTTP request, send a response to the http server
-
-            Response::new()
-                .body(
-                    serde_json::to_vec(&ChatResponse::History {
-                        messages: message_archive.clone(),
-                    })
-                    .unwrap(),
-                )
-                .send()
-                .unwrap();
-        }
-    };
-
-    Ok(())
+pub struct Initialize {
+    tg_token: String,
+    chain: u64,
+    contract: String,
+    min_amount: u64,
+    priv_chat_id: u64,
 }
 
 fn handle_message(
-    our: &Address,
-    message_archive: &mut MessageArchive,
-    channel_id: &mut u32,
+    _our: &Address,
+    api: &Api,
+    worker: &Address,
+    info: &Initialize,
 ) -> anyhow::Result<()> {
-    let message = await_message().unwrap();
+    let message = await_message()?;
 
     match message {
         Message::Response { .. } => {
-            println!("kinogate: got response - {:?}", message);
-            return Ok(());
+            return Err(anyhow::anyhow!("unexpected Response: {:?}", message));
         }
         Message::Request {
             ref source,
             ref body,
             ..
-        } => {
-            // Requests that come from other nodes running this app
-            handle_chat_request(our, message_archive, channel_id, source, body, false)?;
-            // Requests that come from our http server
-            handle_http_server_request(our, message_archive, channel_id, source, body)?;
-        }
-    }
+        } => match serde_json::from_slice(body)? {
+            TgResponse::Update(tg_update) => {
+                let updates = tg_update.updates;
+                // assert update is from our worker
+                if source != worker {
+                    return Err(anyhow::anyhow!(
+                        "unexpected source: {:?}, expected: {:?}",
+                        source,
+                        worker
+                    ));
+                }
 
+                if let Some(update) = updates.last() {
+                    match &update.content {
+                        TgMessage(msg) => {
+                            // get msg contents, and branch on what to send back!
+                            let text = msg.text.clone().unwrap_or_default();
+
+                            // fill in default send_message params, switch content later!
+                            let mut params = SendMessageParams {
+                                chat_id: ChatId::Integer(msg.chat.id),
+                                disable_notification: None,
+                                entities: None,
+                                link_preview_options: None,
+                                message_thread_id: None,
+                                parse_mode: None,
+                                text: "temp".to_string(),
+                                protect_content: None,
+                                reply_markup: None,
+                                reply_parameters: None,
+                                // todo, maybe change api so can ..Default::default()?
+                            };
+
+                            match text.as_str() {
+                                "/start" => {
+                                    let text = format!("hello, to enter this chat, you need at least {:?} of {:?} on chainId {:?} to enter, if you have that, sign the message to get invite link.", info.min_amount, info.contract, info.chain);
+                                    params.text = text;
+                                    api.send_message(&params)?;
+                                }
+                                _ => {
+                                    params.text = "type /start to start.".to_string();
+                                    api.send_message(&params)?;
+                                }
+                            }
+                        }
+                        _ => {
+                            println!("got unhandled tg update: {:?}", update);
+                        }
+                    }
+                }
+            }
+            TgResponse::Error(e) => {
+                println!("error from tg worker: {:?}", e);
+            }
+        },
+    }
     Ok(())
 }
 
 call_init!(init);
 
 fn init(our: Address) {
-    println!("kinogate: begin");
+    println!("kinogate: initialize me to begin!");
 
-    let mut message_archive: MessageArchive = HashMap::new();
-    let mut channel_id = 0;
+    let message = await_message().unwrap();
+    let info = serde_json::from_slice::<Initialize>(message.body()).unwrap();
 
-    // Bind UI files to routes; index.html is bound to "/"
-    serve_ui(&our, "ui", true, true, vec!["/"]).unwrap();
+    let (api, worker) = init_tg_bot(our.clone(), &info.tg_token, None).unwrap();
 
-    // Bind HTTP path /messages
-    bind_http_path("/messages", true, false).unwrap();
+    // serve ui and bind http post path.
+    serve_ui(
+        &our,
+        &format!("{}/pkg/ui/", our.package_id()),
+        false,
+        false,
+        vec![],
+    )
+    .unwrap();
 
-    // Bind WebSocket path
-    bind_ws_path("/", true, false).unwrap();
+    bind_http_path("/signature", false, false).unwrap();
 
     loop {
-        match handle_message(&our, &mut message_archive, &mut channel_id) {
+        match handle_message(&our, &api, &worker, &info) {
             Ok(()) => {}
             Err(e) => {
                 println!("kinogate: error: {:?}", e);
